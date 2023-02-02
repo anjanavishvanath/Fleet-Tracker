ad6374f76b7f48f9c4a035465d37521ad7ca5e7a
SudarakaIT19109190 <it19109190@my.sliit.lk>
2023-02-02 14:35:25 +0530

    added logout controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,3 +75,13 @@ exports.postRegister = (req, res, next) => {
       })
     })
   }
+
+
+exports.logout = (req,res) => {
+  req.logout(() => console.log("User logged out"))
+  req.session.destroy((err) => {
+    if(err) console.log("Error: Failed to destroy the session during logout", err)
+    req.user = null
+    res.redirect("/")
+  })
+}
